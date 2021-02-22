9bc8d448e796553a07e3df1b6a0a93e1755b114a
Ludi <liranforu@gmail.com>
2021-02-22 14:51:21 +0200

    Update main.go
    Flag!

--- OTP/main.go
+++ OTP/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
 	"github.com/AlphaLU/GoAlgos/OTP/otp"
 )
@@ -9,19 +11,35 @@ import (
 var p = fmt.Println
 
 func main() {
-	encMessage := "J2VUOSMMRG2LL"
-	orgMessage := "GRILLEDCHEESE"
-	key := "DLNJDOJKKCY3H"
+	// encMessage := "J2VUOSMMRG2LL"
+	// orgMessage := "GRILLEDCHEESE"
+	// key := "DLNJDOJKKCY3H"
 
-	var msg otp.Message
-	msg.Key = key
-	msg.Message = encMessage
-	msg.Decrypt()
-	p(msg.Result)
+	modeFlag := flag.String("mode", "e", "(e)ncipher/(d)ecipher")
+	keyFlag := flag.String("key", "", "Enter an OTP key")
+	msgFlag := flag.String("message", "", "Enter a message to encipher/decipher")
 
-	var msg2 otp.Message
-	msg2.Key = key
-	msg2.Message = orgMessage
-	msg2.Encrypt()
-	p(msg2.Result)
+	flag.Parse()
+	log.Println(*modeFlag, *keyFlag, *msgFlag)
+	var ret otp.Message
+	ret.Key = *keyFlag
+	ret.Message = *msgFlag
+	if *modeFlag == "e" {
+		ret.Encrypt()
+	} else {
+		ret.Decrypt()
+	}
+
+	p(ret.Result)
+	// var msg otp.Message
+	// msg.Key = key
+	// msg.Message = encMessage
+	// msg.Decrypt()
+	// p(msg.Result)
+
+	// var msg2 otp.Message
+	// msg2.Key = key
+	// msg2.Message = orgMessage
+	// msg2.Encrypt()
+	// p(msg2.Result)
 }
